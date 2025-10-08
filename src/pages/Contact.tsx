8474a54e622b9ebc8afa4230d8d66768f84360f8
afzalgafoor <afzalgafoor@gmail.com>
2025-10-09 02:21:36 +0530

    geo location change

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -417,22 +417,24 @@ const Contact = () => {
           </div>
 
           <div className="max-w-4xl mx-auto">
-            <Card>
-              <CardContent className="p-0">
-                <div className="aspect-video bg-gray-200 rounded-lg overflow-hidden">
-                  <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3024.1282!2d-74.0059413!3d40.7128267!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDDCsDQyJzQ2LjIiTiA3NMKwMDAnMjEuNCJX!5e0!3m2!1sen!2sus!4v1234567890"
-                    width="100%"
-                    height="100%"
-                    style={{ border: 0 }}
-                    allowFullScreen
-                    loading="lazy"
-                    referrerPolicy="no-referrer-when-downgrade"
-                    title="Blue Basket Laundry Location"
-                  ></iframe>
-                </div>
-              </CardContent>
-            </Card>
+<Card>
+  <CardContent className="p-0">
+    <div className="aspect-video bg-gray-200 rounded-lg overflow-hidden">
+      <iframe
+        src="https://www.google.com/maps?q=25.242641,55.298023&hl=en&z=15&output=embed"
+        width="100%"
+        height="100%"
+        style={{ border: 0 }}
+        allowFullScreen
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+        title="Blue Basket Laundry Location"
+      ></iframe>
+    </div>
+  </CardContent>
+</Card>
+
+
           </div>
         </div>
       </section>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -35,8 +35,7 @@ const Home = () => {
     },
     {
       title: "Shoe Care",
-      description:
-        "Professional cleaning and polishing for all footwear",
+      description: "Professional cleaning and polishing for all footwear",
       image: shoeCareImage,
       link: "/services/shoe-care",
     },
@@ -261,7 +260,7 @@ const Home = () => {
               <CardContent className="p-0">
                 <div className="aspect-video bg-gray-200 rounded-lg overflow-hidden">
                   <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3024.1282!2d-74.0059413!3d40.7128267!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDDCsDQyJzQ2LjIiTiA3NMKwMDAnMjEuNCJX!5e0!3m2!1sen!2sus!4v1234567890"
+                    src="https://www.google.com/maps?q=25.242641,55.298023&hl=en&z=15&output=embed"
                     width="100%"
                     height="100%"
                     style={{ border: 0 }}
